2d4f05175a2ddc2432beebd8de7e597996c3e2bc
aberkow <berkowitz.clarinet@gmail.com>
2018-04-15 13:19:21 -0400

    working on footer

--- footer.php
+++ footer.php
@@ -9,6 +9,7 @@
  * @package clarinetist
  */
 
+$site_name = get_bloginfo();
 ?>
 
 	</div><!-- #content -->
@@ -16,22 +17,14 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="widget-area">
 			<?php
-				for ($i = 0; $i < 4; $i++) {
+				for ($i = 0; $i < 5; $i++) {
 					$current = $i;
 					dynamic_sidebar('footer-' . $i);
 				}
 			?>
 		</div>
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'clarinetist' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'clarinetist' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'clarinetist' ), 'clarinetist', '<a href="https://www.adamjberkowitz.com">Adam Berkowitz</a>' );
-			?>
+			<p><?php echo $site_name; ?> - Copyright <?php echo date("Y"); ?></p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
